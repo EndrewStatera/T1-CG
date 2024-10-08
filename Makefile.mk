50c997dfebef72b7fbc52cf216981f482993f7ea
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2024-10-08 11:55:21 -0300

    removidos arquivos duplicados, separada src e obj em suas proprias pastas

--- Makefile.mk
+++ Makefile.mk
@@ -1,9 +1,15 @@
 # Makefile para Windows
 
 PROG = bezier.exe
-FONTES = AnimacaoComBezier.cpp Bezier.cpp InstanciaBZ.cpp ListaDeCoresRGB.cpp Poligono.cpp Ponto.cpp Temporizador.cpp
-OBJETOS = $(FONTES:.cpp=.o)
-CPPFLAGS = -O3 -g -Iinclude # -Wall -g  # Todas as warnings, infos de debug
+
+SRC_DIR = src
+OBJ_DIR = obj
+
+ARQUIVOS = AnimacaoComBezier.cpp Bezier.cpp InstanciaBZ.cpp ListaDeCoresRGB.cpp Poligono.cpp Ponto.cpp Temporizador.cpp
+FONTES = $(addprefix $(SRC_DIR)/, $(ARQUIVOS))
+OBJETOS = $(addprefix $(OBJ_DIR)/, $(ARQUIVOS:.cpp=.o))
+
+CPPFLAGS = -O3 -g -Iinclude -Wall -g  # Todas as warnings, infos de debug
 # Se estiver usando mingw64, troque lib por lib\x64 abaixo
 LDFLAGS = -Llib -lfreeglut -lopengl32 -lglu32 -lm
 CC = g++
